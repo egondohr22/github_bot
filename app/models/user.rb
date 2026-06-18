af2c1119dcd4cb827c9ab109e374de51f469b82f
egondohr22 <egondohr03@gmail.com>
2026-06-18 18:52:51 +0300

    only keep github auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,11 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable, :recoverable,
-         :rememberable, :validatable,
-         :omniauthable, omniauth_providers: [:github]
+  devise :database_authenticatable, :omniauthable, omniauth_providers: [:github]
 
   has_many :installations, dependent: :destroy
   has_many :settings, dependent: :destroy
 
   validates :uid, :github_username, :github_token, presence: true
+  validates :email, presence: true, uniqueness: true
 
   def review_settings
     ReviewSettings.for(self)
@@ -14,11 +13,10 @@ class User < ApplicationRecord
 
   def self.from_omniauth(auth)
     find_or_initialize_by(uid: auth.uid).tap do |user|
-      user.email           = auth.info.email
+      user.email = auth.info.email
       user.github_username = auth.info.nickname
       user.name = auth.info.name
       user.github_token = auth.credentials.token
-      user.password = Devise.friendly_token[0, 20] if user.new_record?
       user.save!
     end
   end
